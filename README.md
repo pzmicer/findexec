86b25aa730846a14c7b83f9d63718969f489746f
pzmicer <pzmicer@gmail.com>
2022-03-25 00:47:24 +0300

    Added --exclude-user flag. README.md modified

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-This is CLI application for finding executables in specified directory
+## About
+
+This CLI application lists ELF files in specified directory and groups them by user
+
+
+## Help
+
+```bash
+$ findexec -h
+findexec 0.1.0
+This application lists ELF files in specified directory and groups them by user
+
+USAGE:
+    findexec [OPTIONS] <TARGET>
+
+ARGS:
+    <TARGET>    Target directory
+
+OPTIONS:
+        --exclude <EXCLUDE>              Exclude files which contains specified string
+        --exclude-user <EXCLUDE_USER>    Exclude files by username
+    -h, --help                           Print help information
+    -o, --output <OUTPUT>                Output type [possible values: json]
+    -r, --recursively                    Recursively list directory
+    -V, --version                        Print version information
+```
+
+
+## Example
+
+```bash
+$ findexec /bin -o json | jq
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs::{self, DirEntry, File}, collections::{HashMap, VecDeque}, path::{PathBuf}, io::Read};
 use std::os::linux::fs::MetadataExt;
-use users::{get_user_by_uid};
+use users::{get_user_by_uid, get_user_by_name};
 use serde::{Serialize, Deserialize};
 use clap::{Parser, ArgEnum};
 
@@ -10,13 +10,14 @@ enum OutputType {
     JSON,
 }
 
-
+/// This application lists ELF files in specified directory and groups them by user
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct FindexecArgs {
-    ///Target directory
+    /// Target directory
     target: String,
 
+    /// Recursively list directory
     #[clap(short, long)]
     recursively: bool,
 
@@ -24,6 +25,11 @@ struct FindexecArgs {
     #[clap(long)]
     exclude: Option<String>,
 
+    /// Exclude files by username
+    #[clap(long)]
+    exclude_user: Option<String>,
+
+    /// Output type
     #[clap(short, long, arg_enum)]
     output: Option<OutputType>,
 }
@@ -31,16 +37,17 @@ struct FindexecArgs {
 #[derive(Serialize, Deserialize)]
 struct Owner {
     uid: u32,
-    name: String,
+    username: String,
     files: Vec<PathBuf>,
     size: u64,
 }
 
-fn list_dir(args: &FindexecArgs) -> Vec<DirEntry> {
+fn list_dir(args: &FindexecArgs) ->Vec<DirEntry> {
     let mut dirs = VecDeque::new();
     dirs.push_back(PathBuf::from(&args.target));
 
     let mut result = Vec::<DirEntry>::new();
+
     while !dirs.is_empty() {
         let elem = dirs.pop_front().expect("BUG: Shouldn't be empty!");
         let contents = match fs::read_dir(elem) {
@@ -55,6 +62,13 @@ fn list_dir(args: &FindexecArgs) -> Vec<DirEntry> {
             
             if let Some(ref exclude) = args.exclude {
                 if entry.file_name().to_str().map(|s| s.contains(exclude)).unwrap_or(false) {
+                    continue;
+                }
+            }
+
+            if let Some(ref exclude_user) = args.exclude_user {
+                let user = get_user_by_name(exclude_user);
+                if user.is_some() && entry.metadata().unwrap().st_uid() == user.unwrap().uid() {
                     continue;
                 }
             }
@@ -77,7 +91,14 @@ fn is_executable(entry: &DirEntry) -> bool {
 }
 
 fn is_elf(path: PathBuf) -> bool {
-    let mut file = File::open(path).unwrap();
+    let mut file = match File::open(&path) {
+        Ok(file) => file, 
+        Err(_) => {
+            println!("Can't open file {}", path.display());
+            return false;
+        }
+    };
+
     let amount = 4;
     let mut buffer = vec![0u8; amount as usize];
     match file.read(&mut buffer) {
@@ -86,53 +107,51 @@ fn is_elf(path: PathBuf) -> bool {
                 return false;
             }
         },
-        Err(e) => {
-            println!("{}", e);
+        Err(_) => {
             return false;
         }
     }
 
-    // check ELF
-    &buffer[1..] == vec![0x45u8, 0x4cu8, 0x46u8]
+    &buffer[1..] == vec![0x45u8, 0x4cu8, 0x46u8] // ELF
 }
 
-fn main() {
-    is_elf(PathBuf::from("/bin/find"));
-
-   let app = FindexecArgs::parse();
-    let contents = list_dir(&app);
-
+fn get_owners(contents: &Vec<DirEntry>) -> Vec<Owner> {
     let mut grouped_by_onwer: HashMap<u32, Vec<&DirEntry>> = HashMap::new();
     
-    for entry in &contents {
+    for entry in contents {
         let metadata = entry.metadata().unwrap();
         grouped_by_onwer.entry(metadata.st_uid()).or_insert(Vec::new()).push(entry);
     }
-    
-    // Serialization
 
     let mut owners: Vec<Owner> = Vec::new();
     for (key, value) in &grouped_by_onwer {
         let files: Vec<PathBuf> = value.iter().map(|&x| x.path()).collect();
         let size = files.iter().map(|e| e.metadata().unwrap().st_size()).sum();
 
-        let owner = if let Some(owner) = get_user_by_uid(*key) {
-                owner
-        } else {
-            println!("Can't find user!");
-            continue
+        let owner = match get_user_by_uid(*key) {
+            Some(owner) => owner,
+            None => continue,
         };
 
         owners.push(Owner { 
             uid: owner.uid(), 
-            name: owner.name().to_str().unwrap().to_owned(), 
+            username: owner.name().to_str().unwrap().to_owned(), 
             files: files,
             size: size,
         });
     }
 
-    owners.sort_by(|a, b| b.files.len().cmp(&a.files.len()));
+    owners
+}
 
+fn main() {
+    let app = FindexecArgs::parse();
+    
+    let contents = list_dir(&app);
+
+    let mut owners = get_owners(&contents);
+
+    owners.sort_by(|a, b| b.files.len().cmp(&a.files.len()));
 
     if let Some(output) = app.output {
         match output {
@@ -142,7 +161,7 @@ fn main() {
         }
     } else {
         for owner in &owners {
-            println!("{}: {:?}", owner.uid, owner.files);
+            println!("{}: {:?}, size = {};", owner.username, owner.files, owner.size);
         }
     }
 }
